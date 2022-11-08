44e6abf38a8e748f974c9fffd5e8ea7e396537a7
YuriDonato <yuridonatosis@gmail.com>
2022-11-07 22:16:45 -0300

    Main Commit

---
+++ app/src/main/java/com/example/awesomemovieapp/MovieViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.awesomemovieapp
+
+import androidx.lifecycle.ViewModel
+
+class MovieViewModel: ViewModel() {
+
+}
---
+++ app/src/main/java/com/example/awesomemovieapp/MyMovieRecyclerViewAdapter.kt
@@ -0,0 +1,60 @@
+package com.example.awesomemovieapp
+
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+import com.example.awesomemovieapp.placeholder.PlaceholderContent.PlaceholderItem
+import com.example.awesomemovieapp.databinding.FragmentItemBinding
+
+/**
+ * [RecyclerView.Adapter] that can display a [PlaceholderItem].
+ * TODO: Replace the implementation with code for your data type.
+ */
+
+interface MovieItemListener{
+    fun onItemSelected(position: Int)
+}
+
+class MyMovieRecyclerViewAdapter(
+    private val values: List<PlaceholderItem>,
+    private val listener: MovieItemListener
+) : RecyclerView.Adapter<MyMovieRecyclerViewAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        return ViewHolder(
+            FragmentItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = values[position]
+        holder.idView.text = item.id
+        holder.contentView.text = item.content
+
+        holder.view.setOnClickListener{
+            listener.onItemSelected(position)
+        }
+    }
+
+    override fun getItemCount(): Int = values.size
+
+    inner class ViewHolder(binding: FragmentItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        val view: View = binding.root
+        val idView: TextView = binding.itemNumber
+        val contentView: TextView = binding.content
+
+        override fun toString(): String {
+            return super.toString() + " '" + contentView.text + "'"
+        }
+    }
+
+}
